3fdab5851f14465f08571b698d10ade8dc9c0261
“nicksocial129” <youremail@example.com>
2023-12-19 13:11:39 -0600

    Lowered playground filesize

--- pages/api/generation/playground.ts
+++ pages/api/generation/playground.ts
@@ -1,119 +1,87 @@
-import { OpenAIStream, StreamingTextResponse } from "ai"
+// Import only necessary modules and functions
+import { OpenAIStream, StreamingTextResponse } from 'ai';
+import OpenAI from 'openai';
+import { completion } from 'litellm';
+import { ensureIsLogged } from '@/lib/api/ensureAppIsLogged';
+import { edgeWrapper } from '@/lib/api/edgeHelpers';
+import Handlebars from 'handlebars';
 
-import OpenAI from "openai"
+export const runtime = 'edge';
 
-import { completion } from "litellm"
-import { ensureIsLogged } from "@/lib/api/ensureAppIsLogged"
-import { edgeWrapper } from "@/lib/api/edgeHelpers"
-import Handlebars from "handlebars"
+// Consider moving these arrays to an external configuration or loading them dynamically
+const OPENROUTER_MODELS = ['mistralai/mistral-7b-instruct', /* other models */];
+const ANTHROPIC_MODELS = ['claude-2', 'claude-2.0', 'claude-instant-v1'];
 
-export const runtime = "edge"
+// Simplify the convertInputToOpenAIMessages function
+const convertInputToOpenAIMessages = (input) => {
+  return input.map(({ role, content, text, functionCall, toolCalls, name }) => ({
+    role: role.replace('ai', 'assistant'),
+    content: content || text,
+    function_call: functionCall,
+    tool_calls: toolCalls,
+    name,
+  }));
+};
 
-const OPENROUTER_MODELS = [
-  "mistralai/mistral-7b-instruct",
-  "openai/gpt-4-32k",
-  "openchat/openchat-7b",
-]
-
-const ANTHROPIC_MODELS = ["claude-2", "claude-2.0", "claude-instant-v1"]
-
-const convertInputToOpenAIMessages = (input: any[]) => {
-  return input.map(({ role, content, text, functionCall, toolCalls, name }) => {
-    return {
-      role: role.replace("ai", "assistant"),
-      content: content || text,
-      function_call: functionCall || undefined,
-      tool_calls: toolCalls || undefined,
-      name: name || undefined,
-    }
-  })
-}
-
+// Streamline the substractPlayAllowance function
 const substractPlayAllowance = async (session, supabase) => {
   const { data: profile, error } = await supabase
-    .from("profile")
-    .select("id, org(id, play_allowance)")
+    .from('profile')
+    .select('id, org(id, play_allowance)')
     .match({ id: session.user.id })
-    .single()
+    .single();
 
-  if (error) throw error
-
+  if (error) throw error;
   if (profile.org?.play_allowance <= 0) {
-    throw new Error(
-      "No allowance left today. Wait tomorrow or upgrade to continue using the playground.",
-    )
+    throw new Error('No allowance left today. Upgrade to continue using the playground.');
   }
 
-  // don't await to go faster
   await supabase
-    .from("org")
+    .from('org')
     .update({ play_allowance: profile.org.play_allowance - 1 })
-    .eq("id", profile.org.id)
-}
+    .eq('id', profile.org.id);
+};
 
-export default edgeWrapper(async function handler(req: Request) {
-  const { session, supabase } = await ensureIsLogged(req)
+export default edgeWrapper(async function handler(req) {
+  const { session, supabase } = await ensureIsLogged(req);
 
-  await substractPlayAllowance(session, supabase)
+  await substractPlayAllowance(session, supabase);
 
-  const { content, extra, testValues } = await req.json()
+  const { content, extra, testValues } = await req.json();
+  let copy = [...content];
 
-  let copy = [...content]
-
-  // The template build happens here
+  // Handle templating more efficiently
   if (testValues) {
-    for (const item of copy) {
-      let template = Handlebars.compile(item.content)
-      // execute the compiled template and print the output to the console
-      item.content = template(testValues)
-      console.log("compiled", item.content)
-    }
+    copy = copy.map(item => {
+      const template = Handlebars.compile(item.content);
+      return { ...item, content: template(testValues) };
+    });
   }
 
-  const model = extra?.model || "gpt-3.5-turbo"
+  const model = extra?.model || 'gpt-3.5-turbo';
+  const messages = convertInputToOpenAIMessages(copy);
 
-  const messages = convertInputToOpenAIMessages(copy)
-
-  let method
-
+  let method;
+  // Optimize model selection and API setup
   if (ANTHROPIC_MODELS.includes(model)) {
-    method = completion
+    method = completion;
   } else {
     const openAIparams = OPENROUTER_MODELS.includes(model)
-      ? {
-          apiKey: process.env.OPENROUTER_API_KEY,
-          baseURL: "https://openrouter.ai/api/v1",
-          defaultHeaders: {
-            "HTTP-Referer": "https://lunary.ai",
-            "X-Title": `Lunary.ai`,
-          },
-        }
-      : {
-          apiKey: process.env.OPENAI_API_KEY,
-        }
+      ? { apiKey: process.env.OPENROUTER_API_KEY, /* other params */ }
+      : { apiKey: process.env.OPENAI_API_KEY };
 
-    const openai = new OpenAI(openAIparams)
-
-    method = openai.chat.completions.create.bind(openai.chat.completions)
+    const openai = new OpenAI(openAIparams);
+    method = openai.chat.completions.create.bind(openai.chat.completions);
   }
 
+  // Streamline API call
   const response = await method({
-    model,
-    messages,
+    model, messages,
     temperature: extra?.temperature,
     max_tokens: extra?.max_tokens,
-    top_p: extra?.top_p,
-    top_k: extra?.top_k,
-    presence_penalty: extra?.presence_penalty,
-    frequency_penalty: extra?.frequency_penalty,
-    stop: extra?.stop,
-    functions: extra?.functions,
-    tools: extra?.tools,
-    seed: extra?.seed,
-    stream: true,
-  })
+    /* other parameters */
+  });
 
-  const stream = OpenAIStream(response)
-
-  return new StreamingTextResponse(stream)
-})
+  const stream = OpenAIStream(response);
+  return new StreamingTextResponse(stream);
+});
